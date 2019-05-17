1157f1773772e210aebd2ce802c06b31788a212d
David <david.benson2826@gmail.com>
2019-05-17 11:03:13 +0100

    Added back end to recyclerView MoodsActivity has an issue with the ArrayList

---
+++ app/src/main/java/uk/ac/yorksj/mobileapplication/foodmood/RecycleViewAdapter.java
@@ -0,0 +1,46 @@
+package uk.ac.yorksj.mobileapplication.foodmood;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewHolder> {
+
+    private LayoutInflater layoutInflater;
+    private ArrayList<String> mood;
+
+    RecycleViewAdapter(ArrayList<String> moods, Context context) {
+
+        this.layoutInflater = LayoutInflater.from(context);
+        this.mood = moods;
+    }
+
+
+    public RecycleViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+
+        View view = layoutInflater.inflate(R.layout.recycler_row, parent, false);
+
+        RecycleViewHolder viewHolder = new RecycleViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecycleViewHolder recycleViewHolder, int position) {
+
+        String content = mood.get(position);
+
+        recycleViewHolder.contentText.setText(content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mood.size();
+    }
+}
