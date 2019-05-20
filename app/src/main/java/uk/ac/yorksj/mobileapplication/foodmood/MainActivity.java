5162b185824a336876d141d0fd4e16f91b567bd5
Sam <sam.holmes@yorksj.ac.uk>
2019-05-20 16:10:14 +0100

    First Commit, Intent added for main screen

--- app/src/main/java/uk/ac/yorksj/mobileapplication/foodmood/MainActivity.java
+++ app/src/main/java/uk/ac/yorksj/mobileapplication/foodmood/MainActivity.java
@@ -1,15 +1,27 @@
 package uk.ac.yorksj.mobileapplication.foodmood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 //FIXME changed exported (in AndroidManifest) to Cuisine for testing purposes. Needs changed back to Main.
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView t;
+        @Override
 
-        //Test - AshleeCW please
+    public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            t = (TextView)findViewById(R.id.textView3);
+            t.setOnClickListener(this);
+        }
+
+    public void onClick(View arg0) {
+        Intent myintent = new Intent(MainActivity.this, SignupActivity.class);
+        startActivity(myintent);
+        }
     }
-}
+
